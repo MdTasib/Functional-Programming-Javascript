d3103857921f6b10046a54d92e2274b2ac94bb39
OhidulAlam <mdtasib442@gmail.com>
2021-03-09 13:16:12 +0600

    First class Function

--- lectures/lecture2.js
+++ lectures/lecture2.js
@@ -9,5 +9,35 @@ var hello = sayName;
 // var fun = sayName('Ohidul Alam');
 // console.log(fun);
 var x = hello;
-var y = x('Ohidul Alam')
+var y = sayName('Ohidul Alam')
 console.log(y);
+
+/////
+
+let arr = [1, 2, 3, 4];
+arr.push(y);
+console.log(arr);
+
+/////
+
+const person = {
+    name: 'Tasib',
+    func: sayName('Ohidul'),
+    print: function () {
+        console.log('Ohidul alam Tasib')
+    }
+}
+console.log(person)
+
+
+//// 
+let sum = 10 + (function () { return 50 })();
+console.log(sum)
+
+///
+
+function wow(name, fun) {
+    return fun(name)
+}
+let result = wow('Ohidul', sayName);
+console.log(result);
