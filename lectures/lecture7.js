73db420e5711ea641f300a657e176e6842e78012
OhidulAlam <mdtasib442@gmail.com>
2021-03-10 13:20:48 +0600

    good()

--- lectures/lecture7.js
+++ lectures/lecture7.js
@@ -1,1 +1,27 @@
 // Javascript Return a Function from Another Function
+function host(name) {
+    return function () {
+        console.log('Hello ' + name);
+    }
+}
+let a = host('Tasib');
+a()
+
+
+
+// *************** //
+
+function greeting(massage) {
+    return function (name) {
+        console.log(massage + ' ' + name);
+    }
+}
+
+let good = greeting('Good Morning, ');
+let hello = greeting('Hello, ')
+
+good()
+hello()
+
+good('Ohidul')
+hello('Ohidul')
